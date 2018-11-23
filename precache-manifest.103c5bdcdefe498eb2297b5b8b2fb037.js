df0fd57c17b14d9f089e8cf679242bc0aedcb22b
Kuba Wojtaszczyk <kuba.wojtaszczyk6@gmail.com>
2018-11-23 09:33:59 +0100

    Updates

---
+++ precache-manifest.103c5bdcdefe498eb2297b5b8b2fb037.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f29868f525abdcbf07de445c08c4aaf",
+    "url": "/Wine-map-of-Poznan/static/media/fb.7f29868f.png"
+  },
+  {
+    "revision": "9ba276650c2364f13ad3",
+    "url": "/Wine-map-of-Poznan/static/css/main.09764452.chunk.css"
+  },
+  {
+    "revision": "f0197c4c866c9023ae866434f181aa81",
+    "url": "/Wine-map-of-Poznan/static/media/bottle.f0197c4c.png"
+  },
+  {
+    "revision": "fda80c87bf90b8260418",
+    "url": "/Wine-map-of-Poznan/static/js/1.fda80c87.chunk.js"
+  },
+  {
+    "revision": "28edfda6a69b042e0c47",
+    "url": "/Wine-map-of-Poznan/static/js/runtime~main.28edfda6.js"
+  },
+  {
+    "revision": "9ba276650c2364f13ad3",
+    "url": "/Wine-map-of-Poznan/static/js/main.9ba27665.chunk.js"
+  },
+  {
+    "revision": "f1677fb3d8879a8571599b176d80d1f5",
+    "url": "/Wine-map-of-Poznan/static/media/google.f1677fb3.png"
+  },
+  {
+    "revision": "5eb5d6d44087e120bf2bdf608c50ca06",
+    "url": "/Wine-map-of-Poznan/static/media/insta.5eb5d6d4.png"
+  },
+  {
+    "revision": "45152241633c937e2c13237e262135b8",
+    "url": "/Wine-map-of-Poznan/static/media/roundImg.45152241.png"
+  },
+  {
+    "revision": "fda80c87bf90b8260418",
+    "url": "/Wine-map-of-Poznan/static/css/1.a209507b.chunk.css"
+  },
+  {
+    "revision": "1bec813f849d17e44db4bc6bf83e1b40",
+    "url": "/Wine-map-of-Poznan/index.html"
+  }
+];
